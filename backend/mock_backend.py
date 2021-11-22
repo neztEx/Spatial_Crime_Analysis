d056d403ce76e9a9717da1d7e21a09394ccefa4e
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-11-21 22:11:43 -0800

    created mock backend for testing frontend purposes

---
+++ backend/mock_backend.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+from json import dumps
+import csv
+
+
+col_name = []
+data = []
+
+with open('crime_data.csv') as csvfile:
+  crime_data = csv.reader(csvfile)
+  
+  for i, row in enumerate(crime_data):
+    for j, datum in enumerate(row):
+      if(i ==0):
+        col_name.append(datum)
+      else:
+        data.append(datum)
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Analysis(Resource):
+  def get(self):
+    return {'analysis': 'test'}
+
+class Map(Resource):
+  def get(self):
+    return {'map': 'test'}
+
+
+api.add_resource(Analysis, '/analysis')
+api.add_resource(Map, '/map')
+
+if __name__ == '__main__':
+  app.run(port='5002')
