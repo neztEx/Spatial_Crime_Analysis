1309b9d49a818f930771a99893751e8c5fe927d8
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-11-22 16:27:41 -0800

    added os specific path for extensions

--- backend/populate_db.py
+++ backend/populate_db.py
@@ -1,11 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.event import listen
 from sqlalchemy.sql import select, func
-
+import platform
+import sys
 
 def load_spatialite(dbapi_conn, connection_record):
   dbapi_conn.enable_load_extension(True)
-  dbapi_conn.load_extension('/usr/lib/x86_64-linux-gnu/mod_spatialite.so')
+  print(platform.system())
+  if(platform.system() == 'Darwin'):
+    dbapi_conn.load_extension('/opt/homebrew/Cellar/libspatialite/5.0.1_1/lib/mod_spatialite.dylib')
+  elif(platform.system() == 'Linux'):
+    dbapi_conn.load_extension('/usr/lib/x86_64-linux-gnu/mod_spatialite.so')
+  else:  
+    print('Unable to assess OS type')
+    sys.exit()
 
 engine = create_engine('sqlite:///gis.db', echo=True)
 listen(engine, 'connect', load_spatialite)
