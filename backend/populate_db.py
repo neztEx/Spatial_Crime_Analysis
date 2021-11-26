1c02ccbe5a3d7e5267b7ef214d1ff135410abe80
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-11-26 10:10:09 -0800

    now inserting into database (takes a while)

--- backend/populate_db.py
+++ backend/populate_db.py
@@ -1,17 +1,19 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, Time
+from sqlalchemy import create_engine, event, Column, Integer, String, Date, DateTime
 from geoalchemy2 import Geometry
 from sqlalchemy.event import listen
 from sqlalchemy.sql import select, func
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+from datetime import datetime, date
 
 import platform
 import sys
 
 
-
 def load_spatialite(dbapi_conn, connection_record):
   dbapi_conn.enable_load_extension(True)
-  print(platform.system())
   if(platform.system() == 'Darwin'):
     dbapi_conn.load_extension('/opt/homebrew/Cellar/libspatialite/5.0.1_1/lib/mod_spatialite.dylib')
   elif(platform.system() == 'Linux'):
@@ -33,8 +35,7 @@ class Crime(Base):
   id = Column(Integer, primary_key=True)
   DR_NO = Column(Integer)
   Date_Rptd = Column(Date)
-  DATE_OCC = Column(Date)
-  TIME_OCC = Column(Time)
+  DATE_TIME_OCC = Column(DateTime)
   AREA = Column(Integer)
   AREA_NAME = Column(String)
   Rpt_Dist_No = Column(Integer)
@@ -57,14 +58,12 @@ class Crime(Base):
   Crm_Cd_4 = Column(Integer)
   LOCATION = Column(String)
   Cross_Street = Column(String)
-  LAT_LON = Column(Geometry(geometry_type='POINT'))
+  LAT_LON = Column(Geometry(geometry_type='POINT', management=True))
 
-  DR_NO,  Date_Rptd,  DATE_OCC,  TIME_OCC,  AREA ,  AREA_NAME,  Rpt_Dist_No ,  Part_1_2 ,  Crm_Cd ,  Crm_Cd_Desc,  Mocodes,  Vict_Age ,  Vict_Sex,  Vict_Descent,  Premis_Cd ,  Premis_Desc,  Weapon_Used_Cd ,  Weapon_Desc, Status,  Status_Desc,  Crm_Cd_1 ,  Crm_Cd_2 ,  Crm_Cd_3 ,  Crm_Cd_4 ,  LOCATION, Cross_Street, LAT_LON
-  def __init__(self, DR_NO,  Date_Rptd,  DATE_OCC,  TIME_OCC,  AREA ,  AREA_NAME,  Rpt_Dist_No ,  Part_1_2 ,  Crm_Cd ,  Crm_Cd_Desc,  Mocodes,  Vict_Age ,  Vict_Sex,  Vict_Descent,  Premis_Cd ,  Premis_Desc,  Weapon_Used_Cd ,  Weapon_Desc, Status,  Status_Desc,  Crm_Cd_1 ,  Crm_Cd_2 ,  Crm_Cd_3 ,  Crm_Cd_4 ,  LOCATION, Cross_Street, LAT_LON):
+  def __init__(self, DR_NO,  Date_Rptd,  DATE_TIME_OCC,  AREA ,  AREA_NAME,  Rpt_Dist_No ,  Part_1_2 ,  Crm_Cd ,  Crm_Cd_Desc,  Mocodes,  Vict_Age ,  Vict_Sex,  Vict_Descent,  Premis_Cd ,  Premis_Desc,  Weapon_Used_Cd ,  Weapon_Desc, Status,  Status_Desc,  Crm_Cd_1 ,  Crm_Cd_2 ,  Crm_Cd_3 ,  Crm_Cd_4 ,  LOCATION, Cross_Street, LAT_LON):
     self.DR_NO = DR_NO
     self.Date_Rptd = Date_Rptd
-    self.DATE_OCC = DATE_OCC
-    self.TIME_OCC = TIME_OCC
+    self.DATE_TIME_OCC = DATE_TIME_OCC
     self.AREA = AREA
     self.AREA_NAME = AREA_NAME
     self.Rpt_Dist_No = Rpt_Dist_No
@@ -90,18 +89,47 @@ class Crime(Base):
     self.LAT_LON = LAT_LON
 
 
-'''
+Crime.__table__.create(engine)
+
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
 import csv
 
-
-conn = engine.connect()
 with open('../dataset/crime_data.csv') as csvfile:
   crime_data = csv.reader(csvfile)
   
   for i, row in enumerate(crime_data):
-    print(len(row))
     if(i != 0):
-      datum = Crime(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21], row[22], row[23], row[24], row[25], row[26]+ row[27] )
-    conn.execute()
-conn.close()
-'''
+      print("HOUR: " + row[3][0:2])
+      print("MINUTE: " + row[3][2:])
+      datum = Crime(int(row[0]),      #DR_NO
+          date(int(row[1][6:10]), int(row[1][0:2]), int(row[1][3:5])),      #Date_Rptd
+          datetime(int(row[2][6:10]), int(row[2][0:2]), int(row[2][3:5]), hour=int(row[3][0:2]), minute=int(row[3][2:])),  #Date_Time_OCC
+          int(row[4]),                #Area
+          row[5],                     #Area_Name
+          int(row[6]),                #Rpt_dist_no
+          int(row[7]),                #part_1_2
+          -1 if row[8] == '' else int(row[8]),                #Crm_cd
+          row[9],                     #Crm_cd_desc
+          row[10],                    #Mocodes
+          -1 if row[11] == '' else int(row[11]),               #vict_age
+          row[12],                    #vict_sex
+          row[13],                    #vict_descent
+          -1 if row[14] == '' else int(row[14]),               #premis_cd
+          row[15],                    #premis_desc
+          -1 if row[16] == '' else int(row[16]),               #weapon_used_cd
+          row[17],                    #weapon_desc
+          row[18],                    #status
+          row[19],                    #status_desc
+          -1 if row[20] == '' else int(row[20]),               #crm_cd_1
+          -1 if row[21] == '' else int(row[21]),               #crm_cd_2
+          -1 if row[22] == '' else int(row[22]),               #crm_cd_3
+          -1 if row[23] == '' else int(row[23]),               #crm_cd_4
+          row[24],                    #Location
+          row[25],                    #Cross_street
+          'POINT('+row[26] + ' ' + row[27] + ')' )         #Lat_lon
+      
+      session.add(datum)
+      session.commit()
