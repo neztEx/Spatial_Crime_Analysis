cb65e5ae62a9ab02ff1ae7b337f962ae8c49c486
Suraj <suraj@Surajs-MBP.lan>
2021-11-28 13:45:41 -0800

    preprocessing part_of_the_day

--- backend/query_builder.py
+++ backend/query_builder.py
@@ -27,7 +27,9 @@ class QueryEngine:
         logger.info("Loading Crime data...")
         self.sql_context = SQLContext(sc)
         self.crime_data_file_path = os.path.join(dataset_path, 'crime_data.csv')
-        self.crime_data = self.sql_context.read.format('com.databricks.spark.csv').options(header='true', inferschema='true').load(self.crime_data_file_path).\
+        self.crime_data = self.sql_context.read.format('com.databricks.spark.csv').options(header='true',
+                                                                                           inferschema='true').load(
+            self.crime_data_file_path). \
             selectExpr(
             "DR_NO",
             "Date_Rptd",
@@ -57,7 +59,50 @@ class QueryEngine:
             "Cross_Street",
             "LAT",
             "LON"
-            )
+        )
+        self.__preprocess_data()
+
+    @staticmethod
+    def __map_function(data_object):
+        """
+            Uses timestamp of the day to return the part of the day
+        """
+        ts = int(data_object.TIME_OCC)
+        if 400 <= ts < 1100:
+            part_of_the_day = "morning"
+        elif 1100 <= ts < 1600:
+            part_of_the_day = "afternoon"
+        elif 1600 <= ts < 2100:
+            part_of_the_day = "evening"
+        else:
+            part_of_the_day = "night"
+        crime_id = data_object.DR_NO
+        date_reported = data_object.Date_Rptd
+        date_occurred = data_object.DATE_OCC
+        time_occurred = data_object.TIME_OCC
+        area = data_object.AREA
+        area_name = data_object.AREA_NAME
+        age = data_object.Vict_Age
+        sex = data_object.Vict_Sex
+        crime_type_id = data_object.Crm_Cd
+        crime_type = data_object.Crm_Cd_Desc
+        location = data_object.LOCATION
+        latitude = data_object.LAT
+        longitude = data_object.LON
+
+        return (crime_id, date_reported, date_occurred, time_occurred, part_of_the_day, area, area_name, age, sex,
+                crime_type_id, crime_type, location, latitude, longitude)
+
+    def __preprocess_data(self):
+        """
+            adds a column on part of the day(morning, afternoon, evening, night) if it doesnt exist
+        """
+        if "part_of_the_day" not in self.crime_data.columns:
+            self.crime_data = self.crime_data.rdd.map(lambda x: QueryEngine.__map_function(x)). \
+                toDF(["crime_id", "date_reported", "date_occurred", "time_occurred", "area", "area_name", "age", "sex",
+                      "crime_type_id", "crime_type", "location", "latitude", "longitude"])
+            self.crime_data.show(10)
+
 
 def __location_based_query(self, location):
     """
