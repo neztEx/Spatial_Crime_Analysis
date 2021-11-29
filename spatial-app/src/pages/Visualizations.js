adbc2690f94ec7a531c2b3bb09a0ee97b5beb070
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-11-29 09:06:06 -0800

    added functionality to hooks

--- spatial-app/src/pages/Visualizations.js
+++ spatial-app/src/pages/Visualizations.js
@@ -3,12 +3,43 @@ import '../App.css'
 import MapView from '../components/GoogleMap';
 import Switch from "react-switch";
 
+import * as QueryServer from '../components/QueryServer'
+
 
 function Visualizations() {
   const [heatMap, setheatMap] = React.useState(false);
+
+  const [crimeData, setCrimeData] = React.useState([])
+
+  const [queryType, setQueryType] = React.useState("location");
+
+  const [queryUpdated, setQueryUpdated] = React.useState(false);
+
+  const options = [
+    {value: 'location', label: 'Location' }
+  ]
+
+  const onSelectChange = React.useEffect(() => {
+    setQueryUpdated(!queryUpdated);
+  }, [queryType])
+
+  const onQueryChange = React.useEffect(() => {
+    switch(queryType){
+      case 'location':
+        QueryServer.location("LOS ANGELES").then(result_json => setCrimeData(result_json))
+        break;
+      default:
+        QueryServer.location("LOS ANGELES").then(result_json => setCrimeData(result_json))
+    }
+  }, [queryUpdated])
+
+  const handleChange = (selectedOption) => {
+    setQueryType(selectedOption);
+  }
+
   return (
         <div>
-          <MapView heatMap={heatMap} />
+          <MapView heatMap={heatMap} crimeData={crimeData}/>
           <Switch onChange={(checked)=> {setheatMap(checked)}} checked={heatMap}/>
         </div>
         
