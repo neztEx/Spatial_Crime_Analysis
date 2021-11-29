0fc0b270cf70dff2404a4d040782ee3ed9bd48f9
Suraj <suraj@Surajs-MBP.lan>
2021-11-28 17:04:36 -0800

    added groupby queries

--- backend/app.py
+++ backend/app.py
@@ -9,12 +9,70 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-@main.route("/location_based_query/<string:location>", methods=["GET"])
-def location_based_query(location):
-    logger.debug(" %s crime data loading....", location)
-    query_results = query_engine.__location_based_query(location)
-    return json.dumps(query_results)
+@main.route("/location_based_query", methods=["GET"])
+def location_based_query():
+    logger.debug(" %s crime data loading....", request.args.get('location'))
+    query_results = query_engine.__location_based_query(request.args.get('location'))
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
 
+@main.route("/year_based_query", methods=["GET"])
+def year_based_query():
+    logger.debug(" crime data between {0} and  {1} loading....".format(request.args.get('start_year'),
+                                                                       request.args.get('end_year')))
+    query_results = query_engine.__year_based_query(request.args.get('start_year'), request.args.get('end_year'))
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
+
+@main.route("/day_part_based_query", methods=["GET"])
+def day_part_based_query(start_year = None, end_year = None, location = None):
+    logger.debug(" crime data on part of the day between {0} and  {1} at location {2} loading....".format(
+                                                                                        request.args.get('start_year')
+                                                                                       , request.args.get('end_year')
+                                                                                       , request.args.get('location')))
+    query_results = query_engine.__generic_attribute_query(start_year, end_year, location, "part_of_the_day")
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
+
+@main.route("/type_of_crime_based_query", methods=["GET"])
+def type_of_crime_based_query(start_year = None, end_year = None, location = None):
+    logger.debug(" crime data on type of crime between {0} and  {1} at location {2} loading....".format(
+                                                                                        request.args.get('start_year')
+                                                                                       , request.args.get('end_year')
+                                                                                       , request.args.get('location')))
+    query_results = query_engine.__generic_attribute_query(start_year, end_year, location, "crime_type")
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
+
+@main.route("/type_of_age_based_query", methods=["GET"])
+def type_of_age_based_query(start_year = None, end_year = None, location = None):
+    logger.debug(" crime data on different ages between {0} and  {1} at location {2} loading....".format(
+                                                                                        request.args.get('start_year')
+                                                                                       , request.args.get('end_year')
+                                                                                       , request.args.get('location')))
+    query_results = query_engine.__generic_attribute_query(start_year, end_year, location, "age")
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
+
+@main.route("/type_of_sex_based_query", methods=["GET"])
+def type_of_sex_based_query(start_year = None, end_year = None, location = None):
+    logger.debug(" crime data on different sex between {0} and  {1} at location {2} loading....".format(
+                                                                                        request.args.get('start_year')
+                                                                                       , request.args.get('end_year')
+                                                                                       , request.args.get('location')))
+    query_results = query_engine.__generic_attribute_query(start_year, end_year, location, "sex")
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
+
+@main.route("/month_based_query", methods=["GET"])
+def month_based_query(start_year = None, end_year = None, location = None):
+    logger.debug(" crime data on different months between {0} and  {1} at location {2} loading....".format(
+                                                                                        request.args.get('start_year')
+                                                                                       , request.args.get('end_year')
+                                                                                       , request.args.get('location')))
+    query_results = query_engine.__generic_attribute_query(start_year, end_year, location, "month")
+    response = query_results.toJSON().map(lambda j: json.loads(j)).collect()
+    return json.dumps(response)
 
 
 def create_app(spark_context, dataset_path):
--- backend/query_builder.py
+++ backend/query_builder.py
@@ -60,6 +60,7 @@ class QueryEngine:
             "LAT",
             "LON"
         )
+        logger.info("Preprocessing the data...")
         self.__preprocess_data()
 
     @staticmethod
@@ -89,9 +90,10 @@ class QueryEngine:
         location = data_object.LOCATION
         latitude = data_object.LAT
         longitude = data_object.LON
+        month = data_object.DATE_OCC[0: 2]
 
         return (crime_id, date_reported, date_occurred, time_occurred, part_of_the_day, area, area_name, age, sex,
-                crime_type_id, crime_type, location, latitude, longitude)
+                crime_type_id, crime_type, location, latitude, longitude, month)
 
     def __preprocess_data(self):
         """
@@ -99,20 +101,66 @@ class QueryEngine:
         """
         if "part_of_the_day" not in self.crime_data.columns:
             self.crime_data = self.crime_data.rdd.map(lambda x: QueryEngine.__map_function(x)). \
-                toDF(["crime_id", "date_reported", "date_occurred", "time_occurred", "area", "area_name", "age", "sex",
-                      "crime_type_id", "crime_type", "location", "latitude", "longitude"])
+                toDF(["crime_id", "date_reported", "date_occurred", "time_occurred", "part_of_the_day", "area",
+                      "area_name", "age", "sex",
+                      "crime_type_id", "crime_type", "location", "latitude", "longitude", "month"])
             self.crime_data.show(10)
 
-
 def __location_based_query(self, location):
+    """
+        Helper function on location based query
+    """
+    logger.info("Executing Location Query...")
+    return __location_based_query_helper(self.crime_data, location)
+
+
+def __location_based_query_helper(crime_data, location):
     """
         Loads the crime data on location basis
     """
     logger.info("Running Location Query...")
-    location_query_results = self.crime_data.where(self.crime_data.LOCATION == location)
+    location_query_results = crime_data.where(crime_data.location == location)
     location_query_results.show(10)
-    results = location_query_results.toJSON().map(lambda j: json.loads(j)).collect()
-    return results
+    return location_query_results
+
+def __year_based_query(self, start_year, end_year):
+    """
+        Helper function on year based query
+    """
+    logger.info("Executing Year Query...")
+    return __year_based_query_helper(self.crime_data, start_year, end_year)
+
+def __year_based_query_helper(crime_data, start_year, end_year):
+    """
+        Loads the crime data on year basis
+    """
+    logger.info("Running Year Query...")
+    if start_year is None:
+        start_year = "2010"
+    if end_year is None:
+        end_year = "2021"
+    year_query_results = crime_data.where((start_year <= f.substring(crime_data.date_occurred, 7, 4)) &
+                                               (f.substring(crime_data.date_occurred, 7, 4) <= end_year))
+    year_query_results.show(10)
+    return year_query_results
+
+def __generic_attribute_query(self, start_year, end_year, location, grouping_attribue):
+    """
+        Loads the crime data grouping on different attributes
+    """
+    logger.info("Running {} Query...".format(grouping_attribue))
+    day_part_query_results = self.crime_data
+
+    if start_year is not None or end_year is not None:
+        day_part_query_results = __year_based_query_helper(day_part_query_results, start_year, end_year)
+    if location is not None:
+        day_part_query_results = __location_based_query_helper(day_part_query_results, location)
+
+    part_of_the_day_query_results = day_part_query_results.groupBy(grouping_attribue).count()
+    part_of_the_day_query_results.show()
+    return part_of_the_day_query_results
 
 
 QueryEngine.__location_based_query = __location_based_query
+QueryEngine.__year_based_query = __year_based_query
+QueryEngine.__generic_attribute_query = __generic_attribute_query
