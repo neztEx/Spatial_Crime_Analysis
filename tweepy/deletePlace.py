920a9a32164279ac144ea71be314de688920317a
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-11-30 22:36:42 -0800

    also removed tweet

--- tweepy/deletePlace.py
+++ tweepy/deletePlace.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('../dataset/tweetsWithSentiment.csv')
-df.drop(df.columns[[0,1,2,4,8,9]],axis=1,inplace=True)
+df.drop(df.columns[[0,1,2,4,7,8,9]],axis=1,inplace=True)
 print(df.head())
 df.to_csv('../dataset/cleanedSentimentTweets.csv')
